c4b8f6100c698b400dae52cac84170e0ba1339a6
James Eckersall <james.eckersall@gmail.com>
2015-10-13 07:12:07 +0100

    fixed client class

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,6 +1,28 @@
 class nfs::client (
+  $package_ensure = 'installed',
+  $package_name   = undef,
 ) {
 
-  # Nothing done here yet
+  validate_string($package_ensure)
+  validate_string($package_name)
+
+  if ( $package_ensure == undef ) {
+    fail ('$package_ensure cannot be undef.')
+  }
+
+  if ( $package_name == undef ) {
+
+    case $::osfamily {
+      'RedHat': { $real_package_name = 'nfs-utils' }
+      'Debian': { $real_package_name = 'nfs-common' }
+      default: { fail("osfamily ${::osfamily} is not supported.") }
+    }
+  } else {
+    $real_package_name = $package_name
+  }
+
+  package { $real_package_name:
+    ensure => $package_ensure,
+  }
 
 }
